a2b26bf45ee73c35fd6ce1abf18cfcdb05265d0f
Matthew Lundquist <lundquistecologylab@gmail.com>
2022-12-03 11:27:27 -0500

    import data example

---
+++ lessons/data_management/buit_in_data.r
@@ -0,0 +1,9 @@
+# Importing built in data
+
+data(iris) # This imports the built-in Iris dataset
+
+print(iris$Species) # Example code, printing the species of Iris in the dataset
+
+# List all built-in datasets
+
+data()
