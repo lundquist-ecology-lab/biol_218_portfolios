1145d2a864668885e4404c8bdfec1a0a311d35ff
Matthew Lundquist <lundquistecologylab@gmail.com>
2022-12-12 09:17:45 -0500

    plotting lesson

--- lessons/02_displaying_data/basic_plotting/scatter_plot.r
+++ lessons/02_displaying_data/basic_plotting/scatter_plot.r
@@ -6,7 +6,7 @@ data(iris)
 x <- iris$Sepal.Length
 y <- iris$Sepal.Width
 
-# R will automatically detect the data type and produce 
+# R will automatically detect the data type and produce
 # a scatter plot
 plot(x, y, pch = 16, col = "#69b3a2", xlab = "Sepal Length",
      ylab = "Sepal Width", main = "Iris sepal length vs. width",
---
+++ lessons/02_displaying_data/basic_plotting/time_series.r
@@ -0,0 +1,5 @@
+# Time series in R
+data(co2)
+
+plot(co2, col = "#69b3a2", xlab = "year",
+    ylab = expression(CO[2] ~ concentration))
